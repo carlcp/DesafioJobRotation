7151be1fe0d6bf865442af5cbca017f6418a618b
carlcp <carlinhosmessiaugusto2@gmail.com>
2022-07-18 17:27:41 -0300

    2

---
+++ src/main/java/Tests/Desafio5.java
@@ -0,0 +1,24 @@
+package Tests;
+
+import javax.swing.JOptionPane;
+
+public class Desafio5 {
+    public static void main(String[] args) {
+        String str = JOptionPane.showInputDialog("Digite uma palavra ");
+	System.out.println(str);
+	char[] ch = str.toCharArray();
+	int begin = 0;
+	int end = ch.length-1;
+        char temp;
+	    while(end>begin){
+	        temp = ch[begin];
+	        ch[begin] = ch[end];
+	        ch[end] = temp;
+	        end--;
+	        begin++;
+	    }
+	    str = new String(ch);
+	    System.out.println(str);
+	}
+    }
+
